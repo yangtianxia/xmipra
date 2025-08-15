1875459e8dbd92e130702f2f60de5ada56baae78
yangtianxia <1304082170@qq.com>
2025-08-15 22:43:58 +0800

    chore(xmipra) 优化use/selector方法

--- packages/xmipra/src/use/selector/query-all.ts
+++ packages/xmipra/src/use/selector/query-all.ts
@@ -22,16 +22,16 @@ export function useSelectorQueryAll(
   /** 是否缓存 */
   let cached = false
   /** 是否已回调 */
-  let calledBack = false
+  let called = false
 
   const execCallback = (results: DOMRect[]) => {
-    if (callback && (alwaysCallback || !calledBack)) {
+    if (callback && (alwaysCallback || !called)) {
       callback(results)
-      calledBack = true
+      called = true
     }
   }
 
-  const getBoundingClientRect = async () => {
+  const getBoundingClientRect = () => {
     return new Promise<DOMRect[]>((resolve, reject) => {
       if (cache && cached) {
         resolve(rects.value)
